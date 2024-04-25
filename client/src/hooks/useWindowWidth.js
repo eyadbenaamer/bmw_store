cf687f80c6467dae1f409357534c4a0c23cd2991
eyadbinamir <eyad.y.binamir@gmail.com>
2024-04-25 08:51:06 +0200

    perf: unbind resize event when it is done

--- client/src/hooks/useWindowWidth.js
+++ client/src/hooks/useWindowWidth.js
@@ -1,8 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 export const useWindowWidth = () => {
   let [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  window.addEventListener("resize", () => {
-    setWindowWidth(window.innerWidth);
-  });
+  useEffect(() => {
+    const resize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    // Bind the event listener
+    window.addEventListener("resize", resize);
+    // Unbind the event listener on clean up
+    return () => window.removeEventListener("resize", resize);
+  }, []);
+
   return windowWidth;
 };
